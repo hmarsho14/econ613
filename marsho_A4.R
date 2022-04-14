97b9a89adb6d87dc0918313dfdeb38ba70443c11
hmarsho14 <75851181+hmarsho14@users.noreply.github.com>
2022-04-13 20:12:32 -0400

    A4

---
+++ marsho_A4.R
@@ -0,0 +1,242 @@
+#=========================================================================
+# Econ 613 Homework 4: Censoring and Panel Data
+# Hannah Marsho
+#=========================================================================
+
+library(tidyverse)
+library(readr)
+library(ggplot2)
+
+setwd("~/Desktop/econ613_wd/A4/Data")
+dat <- read.csv('dat_A4.csv')
+dat_panel <- read.csv('dat_A4_panel.csv')
+
+#=========================================================================
+# Exercise 1: Preparing the Data
+#=========================================================================
+
+# a) Create additional variable for the age of the agent "age", total work experience measured in years
+# "work_exp".
+
+# Let's calculate age first. I interpret that this question could mean age at time of initial 
+# survey (in 1997) or age at time of final survey/present day (in 2019). I'll calculate both to be safe.
+
+yearofsurvey = 1997
+dat <- dat %>% mutate(age_initial = yearofsurvey - KEY_BDATE_Y_1997)
+yearofsurvey = 2019
+dat <- dat %>% mutate(age_final = yearofsurvey - KEY_BDATE_Y_1997)
+
+# Now let's calculate work experience. I interpret this question as work experience (in weeks) collected 
+# from all jobs in total. So, even if the individual had multiple jobs at once (overlap), the experience (in weeks)
+# will still be calculated separately for each position.
+
+num_obs <- nrow(dat)
+dat <- dat %>% mutate(work_exp = 0)
+for (i in 1:num_obs) {
+  all_work_exp <- sum(dat$CV_WKSWK_JOB_DLI.01_2019[i], dat$CV_WKSWK_JOB_DLI.02_2019[i], dat$CV_WKSWK_JOB_DLI.03_2019[i],
+                 dat$CV_WKSWK_JOB_DLI.04_2019[i], dat$CV_WKSWK_JOB_DLI.05_2019[i], dat$CV_WKSWK_JOB_DLI.06_2019[i],
+                 dat$CV_WKSWK_JOB_DLI.07_2019[i], dat$CV_WKSWK_JOB_DLI.08_2019[i], dat$CV_WKSWK_JOB_DLI.09_2019[i],
+                 dat$CV_WKSWK_JOB_DLI.10_2019[i], dat$CV_WKSWK_JOB_DLI.11_2019[i], na.rm = TRUE)
+  dat$work_exp[i] <- all_work_exp / 52
+}
+
+# b) Create additional education variables indicating total years of schooling from all 
+# variables related to education in our data set.
+
+# We can create education variables based on: biological father's highest grade completed, biological
+# mother's highest grade completed, residential father's highest grade completed, residential
+# mother's highest grade completed, and highest degree ever received.
+
+to_drop1 <- which(dat$CV_HGC_BIO_DAD_1997 == 95)
+to_drop2 <- which(dat$CV_HGC_BIO_MOM_1997 == 95)
+to_drop3 <- which(dat$CV_HGC_RES_DAD_1997 == 95)
+to_drop4 <- which(dat$CV_HGC_RES_MOM_1997 == 95)
+dat$CV_HGC_BIO_DAD_1997[to_drop1] = NA
+dat$CV_HGC_BIO_MOM_1997[to_drop2] = NA
+dat$CV_HGC_RES_DAD_1997[to_drop3] = NA
+dat$CV_HGC_RES_MOM_1997[to_drop4] = NA
+dat <- dat %>% mutate(average_grade_parent = rowSums(dat[, 8:11], na.rm = TRUE) / 4)
+
+# the final additional education variable created below, highest_degree_received, is pretty subjective. 
+# I am not counting preschool or kindergarten as part of years of schooling since we have no 
+# information on them. Additionally, I am assuming that the other degree programs are completed in 
+# the commonly known amount. For example, bachelor's is 4 years, master's is 2 years, and 
+# PhD/professional degree is 5 years here. I also assume that the individual went straight to the 
+# program from which they received their highest degree (for example, if they completed PhD, I am 
+# assuming they went straight from a four-year bachelor's to PhD program, with no master's in-between).
+
+dat <- dat %>% mutate(highest_degree_received = case_when(dat$YSCH.3113_2019 == 1 ~ "<12",
+                                                          dat$YSCH.3113_2019 == 2 ~ "~12",
+                                                          dat$YSCH.3113_2019 == 3 ~ "12",
+                                                          dat$YSCH.3113_2019 == 4 ~ "14",
+                                                          dat$YSCH.3113_2019 == 5 ~ "16",
+                                                          dat$YSCH.3113_2019 == 6 ~ "18",
+                                                          dat$YSCH.3113_2019 == 7 ~ "21",
+                                                          dat$YSCH.3113_2019 == 8 ~ "21"))
+
+# c) Provide the following visualizations.
+
+# For the following questions, I am interpreting "number of children" to mean number of biological 
+# children under 18 living in the household at the time of the survey. 
+
+# i) Plot the income data (where income is positive) by i) age groups, ii) gender groups, and iii) number of children groups.
+
+# For the age and number of children groups, I calculate average incomes for each group and then plot that. For the gender groups, since
+# there are only two options here, I simply made histograms to show the income distribution for each gender.
+
+dat_income_filtered <- dat %>% filter(dat$YINC_1700_2019 != 0 & dat$YINC_1700_2019 != 'NA')
+dat_filtered_age_group <- dat_income_filtered %>% group_by(age_final) %>%
+  summarize_at(vars(YINC_1700_2019), list(average_income = mean))
+ggplot(dat_filtered_age_group, aes(x = age_final, y = average_income)) + geom_point() + labs(x = "Age", y = "Average Income") 
+
+dat_filtered_gender_male <- dat_income_filtered %>% filter(dat_income_filtered$KEY_SEX_1997 != 0 & dat_income_filtered$KEY_SEX_1997 != 2)
+dat_filtered_gender_female <- dat_income_filtered %>% filter(dat_income_filtered$KEY_SEX_1997 != 0 & dat_income_filtered$KEY_SEX_1997 != 1)
+hist(dat_filtered_gender_male$YINC_1700_2019, main = "Income - Male")
+hist(dat_filtered_gender_female$YINC_1700_2019, main = "Income - Female")
+
+dat_filtered_numchildren_group <- dat_income_filtered %>% filter(dat_income_filtered$CV_BIO_CHILD_HH_U18_2019 != 'NA') %>% 
+  group_by(CV_BIO_CHILD_HH_U18_2019) %>% summarize_at(vars(YINC_1700_2019), list(average_income = mean))
+ggplot(dat_filtered_numchildren_group, aes(x = CV_BIO_CHILD_HH_U18_2019, y = average_income)) + geom_point() + labs(x = "Number of Children", y = "Average Income") 
+
+# ii) Table the share of "0" in the income data by i) age groups, ii) gender groups, iii) number of
+# children and marital status.
+
+dat$YINC_1700_2019[is.na(dat$YINC_1700_2019)] <- 0
+dat_age_group <- dat %>% group_by(age_final) %>% summarise(N = n(), num_zeros = length(which(YINC_1700_2019 == 0))) %>% 
+  mutate(share_zeros = num_zeros / N)
+dat_gender_group <- dat %>% group_by(KEY_SEX_1997) %>% 
+  summarise(N = n(), num_zeros = length(which(YINC_1700_2019 == 0))) %>% 
+  mutate(share_zeros = num_zeros / N)
+dat_numchildrenmarital_group <- dat %>% group_by(CV_BIO_CHILD_HH_U18_2019, CV_MARSTAT_COLLAPSED_2019) %>% 
+  summarise(N = n(), num_zeros = length(which(YINC_1700_2019 == 0))) %>% mutate(share_zeros = num_zeros / N) %>%
+  filter(CV_BIO_CHILD_HH_U18_2019 != 'NA' & CV_MARSTAT_COLLAPSED_2019 != 'NA')
+
+# iii) Interpret the visualizations from above.
+
+# Return to this later.
+
+#=========================================================================
+# Exercise 2: Heckman Selection Model
+#=========================================================================
+
+# Using the variables created above, estimate the following models.
+
+# a) Specify and estimate an OLS model to explain the income variable (where income is positive).
+
+reg1 <- lm(YINC_1700_2019 ~ age_final + work_exp + average_grade_parent + 
+             highest_degree_received + KEY_SEX_1997 + CV_BIO_CHILD_HH_U18_2019 + 
+             CV_MARSTAT_COLLAPSED_2019, data = dat_income_filtered)
+summary(reg1)
+
+# i) Interpret the estimation results.
+
+# Return to this later.
+
+# ii) Explain why there might be a selection problem when estimating an OLS this way.
+
+# There are many 0's and NA's in the data, so OLS estimates might be biased.
+# Return to this later.
+
+# b) Explain why the Heckman model can deal with the selection problem.
+
+# Return to this later.
+
+# c) Estimate a Heckman selection model (Please write down the likelihood and optimize the two-stage Heckman model).
+# Interpret the results from the Heckman selection model and compare the results to OLS results. Why does there
+# exist a difference?
+
+dat <- dat %>% mutate(intercept = 1, income_exists = 0)
+dat$income_exists[which(dat$YINC_1700_2019 > 0)] <- 1
+
+# Our Probit Function
+
+flikelihood <- function(par, intercept, x1, x2, x3, x4, x5, x6, x7, income_exists) {
+  yhat <- par[1] * intercept + par[2] * x1 + par[3] * x2 + par[4] * x3 + par[5] * x4 +
+    par[6] * x5 + par[7] * x6 + par[7] * x7
+  prob <- pnorm(yhat)
+  prob[prob > 0.999999] <- 0.999999
+  prob[prob < 0.000001] <- 0.000001
+  like <- income_exists * log(prob) + (1 - income_exists) * log(1 - prob)
+  return(-sum(like))
+}
+
+predictor <- function(par, intercept, x1, x2, x3, x4, income_exists) {
+  yhat <- par[1] * intercept + par[2] * x1 + par[3] * x2 + par[4] * x3 + par[5] * x4 +
+    par[6] * x5 + par[7] * x6 + par[7] * x7 
+  return(yhat)
+}
+
+intercept <- dat$intercept
+x1 <- dat$age_final
+x2 <- dat$work_exp
+x3 <- dat$average_grade_parent
+x4 <- dat$highest_degree_received
+x5 <- dat$KEY_SEX_1997
+x6 <- dat$CV_BIO_CHILD_HH_U18_2019
+x7 <- dat$CV_MARSTAT_COLLAPSED_2019
+income_exists = dat$income_exists
+
+# Should the following lines include intercept in their formula?
+
+start <- runif(5, -0.5, 0.5)
+result  <- optim(start, fn = flikelihood, method="BFGS", control = list(trace = 6, REPORT = 1, maxit = 1000),
+                 x1 = x1, x2 = x2, x3 = x3, x4 = x4, x5 = x5, x6 = x6, x7 = x7, income_exists = dat$income_exists, hessian = TRUE)
+result$par
+
+# Use Probit package to check result
+
+reg1 <- glm(income_exists ~ x1 + x2 + x3 + x3 + x4 + x5 + x6 + x7, family = binomial(link = "probit"))
+summary(reg1)
+reg1$coefficients
+predictor <- predictor(result$par, x1, x2, x3, x4, x5, x6, x7, income_exists)
+invM_ratio <- dnorm(predictor) / pnorm(predictor)
+Heckman_reg <- lm(dat$YINC_1700_2019 ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + invM_ratio)
+
+#=========================================================================
+# Exercise 3: Censoring
+#=========================================================================
+
+# Note that the "YINC-1700" variable is censored because of privacy issues. In other 
+# words, high wages are top-coded in this data set.
+
+# a) Plot a histogram to check whether the distribution of the income variable is
+# affected. What might be the censored value here?
+
+hist(dat_income_filtered$YINC_1700_2019, main = "Income")
+
+# From checking the data set, we can see that the highest possible value is $100,000
+# for income. So the top-coded/censored value is simply $100,000.
+
+# b) Propose a model to deal with the censoring problem.
+
+# c) Estimate the appropriate model with the censored data.
+
+# d) Interpret the results above and compare to those when not correcting for 
+# the censored data.
+
+#=========================================================================
+# Exercise 4: Panel Data
+#=========================================================================
+
+# In the second part, we use the panel dimension of NLSY97 data.
+
+# We are interested in the effect of education, marital status, experience,
+# and education on wages.
+
+# a) Explain the potential ability bias when trying to explain to understand the 
+# determinants of wages.
+
+# b) Exploit the panel dimension of the data to propose a model to correct for the
+# ability bias. Estimate the model using the following strategy.
+
+# i) Within estimator.
+
+# ii) Between estimator.
+
+# iii) Difference (any) Estimator.
+
+# c) Interpret the results from each model and explain why different models yield 
+# different parameter estimates.
+
+
+
